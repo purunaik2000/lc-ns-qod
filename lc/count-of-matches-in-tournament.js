b3fd9eb74814cedf7e17bc5326033e59779e6c2d
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2023-12-05 05:59:49 +0530

    05-dec-2023 qod

---
+++ lc/count-of-matches-in-tournament.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function(n) {
+    let count = 0;
+    while(n>1) {
+        count += parseInt(n/2);
+        n = parseInt(n/2) + n%2;
+    }
+    return count;
+};
