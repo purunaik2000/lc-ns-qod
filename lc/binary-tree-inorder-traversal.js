2e0b0f7bdaeeaf0f9a68f612793a080578c45b86
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2023-12-09 06:22:09 +0530

    09-dec-2023 qod

---
+++ lc/binary-tree-inorder-traversal.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let ans = [];
+    if(!root) return ans;
+    ans.push(...inorderTraversal(root.left));
+    ans.push(root.val);
+    ans.push(...inorderTraversal(root.right));
+    return ans;
+};
---
+++ ns/Nearest Power of Two.js
@@ -0,0 +1,34 @@
+process.stdin.setEncoding('utf8');
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function solution(n){
+    // Code here
+    let pow = 1;
+    while(pow<n) pow*=2;
+    if(pow-n < (n-pow/2)) return pow;
+    return pow/2;
+ }
+    
+function main() {
+    // let t = parseInt(readLine());
+    let t = 1;
+    while(t--){
+        let [n] = readLine().split(' ').map(Number);
+        let res = solution(n);
+        console.log(res);
+    }
+}
