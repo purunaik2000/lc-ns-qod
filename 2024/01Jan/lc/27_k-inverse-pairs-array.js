a7e7821538dea6d0cdce0960ff36621428593db2
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2024-01-27 23:20:26 +0530

    27-jan-2024 qod

---
+++ 2024/01Jan/lc/27_k-inverse-pairs-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kInversePairs = function(n, k) {
+    let dp = new Array(1001).fill(0).map(() => new Array(1001).fill(0));
+    dp[0][0] = 1;
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j <= k; j++) {
+            for (let x = 0; x <= Math.min(j, i - 1); x++) {
+                if (j - x >= 0) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % (1e9+7);
+                }
+            }
+        }
+    }
+
+    return dp[n][k];
+};
