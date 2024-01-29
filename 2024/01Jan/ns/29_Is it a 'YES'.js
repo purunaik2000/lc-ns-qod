565e11fd17aa2bce336d3ffa96d519e3e46ff512
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2024-01-29 21:40:35 +0530

    29-jan-2024 qod

---
+++ 2024/01Jan/ns/29_Is it a 'YES'.js
@@ -0,0 +1,31 @@
+process.stdin.setEncoding('utf8');
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function solution(s){
+    // Code here
+    return s.toLowerCase() == 'yes' ? 'Yes' : 'No';
+ }
+    
+function main() {
+    // let t = parseInt(readLine());
+    let t = 1;
+    while(t--){
+        let s = readLine();
+        let res = solution(s);
+        console.log(res);
+    }
+}
