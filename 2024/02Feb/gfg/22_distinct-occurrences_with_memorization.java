1d91262a591fba06373117b595d6dfbc9951f70e
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2024-02-22 23:05:54 +0530

    22-feb-2024 qod

---
+++ 2024/02Feb/gfg/22_distinct-occurrences_with_memorization.java
@@ -0,0 +1,24 @@
+class Solution
+{
+    int  subsequenceCount(String s, String t)
+    {
+	    // Your code here	
+	    int[][] dp = new int[s.length()+1][t.length()+1];
+	    
+	    for(int i=0;i<=s.length();i++) {
+	        dp[i][0] = 1;
+	    }
+	    
+	    for(int i=1;i<=s.length();i++) {
+	        for(int j=1;j<=t.length();j++) {
+	            dp[i][j] = dp[i-1][j];
+	            if(s.charAt(i-1) == t.charAt(j-1)) {
+	                dp[i][j] += dp[i-1][j-1];
+	                dp[i][j] %= 1000000007;
+	            }
+	        }
+	    }
+	    
+	    return dp[s.length()][t.length()];
+    }
+}
